6d0eca942bd00746268c31dec338a6713e926d51
Nonelela4980 <nonelelacele@gmail.com>
2024-03-09 22:52:11 +0200

    initial project setup

---
+++ nosql-explorer/src/renderer/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <div>
+      <span className="text-4xl text-blue-700">hello</span>
+    </div>
+  )
+}
+
+export default App
